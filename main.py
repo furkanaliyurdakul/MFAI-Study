0a9e84d44e774407e1eca06c44b0e2bc2a6a20ce
Please <furkanaliyurdakul@gmail.com>
2026-01-17 04:43:49 +0100

    Revert key parameter - st.image doesn't support it

--- main.py
+++ main.py
@@ -1031,8 +1031,7 @@ elif st.session_state.current_page == "learning":
                         img = Image.open(slide_path)
                     
                     caption = str(slide_path.name) if DEV_MODE else None
-                    # Use key to force re-render when slide changes
-                    st.image(img, caption=caption, use_column_width=True, key=f"slide_img_{idx}")
+                    st.image(img, caption=caption, use_column_width=True)
                 except Exception as e:
                     st.error(f"Unable to display slide image: {str(e)}")
                     if DEV_MODE:
